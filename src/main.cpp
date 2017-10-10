e4d99bae0f759b8efd59060c6667742da2da0593
LegoMaster616 <andrew.afflitto@gmail.com>
2017-10-10 18:56:22 -0400

    set up basic web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "";
+const char* password = "";
+MDNSResponder mdns;
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  Serial.println("root");
+  server.send(200, "text/plain", "hello world");
+}
+
+void handleNotFound() {
+  Serial.println("not found");
+  server.send(404, "text/plain", "404: not found");
+}
 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("Connected");
+  Serial.println("IP: " + WiFi.localIP().toString());
+
+  if(mdns.begin("esp8266", WiFi.localIP()))
+  {
+    Serial.println("mdns started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("server started");
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  server.handleClient();
 }
