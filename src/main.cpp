d6dc0208cb0c8ce1efe8a3f7f0a2763250053b72
LegoMaster616 <andrew.afflitto@gmail.com>
2017-10-11 16:46:33 -0400

    cleaned up and documented code

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,20 @@
 #include "pages.h"
 #include "keys.h"
 
-#define BUTTON_DELAY 750
+#define BUTTON_DELAY 750 //Time in milliseconds that the relay is held HIGH
 
-const int relayPin = LED_BUILTIN;
+//const int relayPin = D1; //Pin used for the relay
+const int relayPin = LED_BUILTIN; //Set to LED_BUILTIN for now for debugging
 
-long relayStartTime = 0;
+long relayStartTime = 0; //Used to keep track of how long the relay has been on
 
+//Device Networking
 MDNSResponder mdns;
-
 ESP8266WebServer server(80);
 WebSocketsServer ws = WebSocketsServer(81);
 ThingerWifi thing(thingerUserName, thingerDeviceID, thingerDeviceCredential);
 
+//Turns on the relay. Will turn off after BUTTON_DELAY ms
 void activateRelay()
 {
   relayStartTime = millis();
@@ -31,6 +33,7 @@ void activateRelay()
   }
 }
 
+//Turns off relay if needed
 void updateRelay()
 {
   if(millis() > relayStartTime + BUTTON_DELAY)
@@ -43,6 +46,7 @@ void updateRelay()
   }
 }
 
+//URL Handlers
 void handleRoot() {
   Serial.println("root");
   server.send(200, "text/html", rootHTML);
@@ -58,6 +62,7 @@ void handleNotFound() {
   server.send(404, "text/plain", "404: not found");
 }
 
+//Websocket handler
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
   switch(type) {
     case WStype_DISCONNECTED:
@@ -77,12 +82,16 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
   }
 }
 
+
 void setup() {
+  //Set up relay pin
   pinMode(relayPin, OUTPUT);
   digitalWrite(relayPin, LOW);
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
 
+  Serial.begin(115200);
+
+  //Connect to WiFi
+  WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -92,29 +101,33 @@ void setup() {
   Serial.println("Connected");
   Serial.println("IP: " + WiFi.localIP().toString());
 
+  //Add Thinger.io input resource
+  //Will call activateRelay() when a web request is sent to Thinger.io
   thing["toggle"] << [](pson& in){
     activateRelay();
   };
 
+  //Start MDNS
   if(mdns.begin("esp8266", WiFi.localIP()))
   {
     Serial.println("mdns started");
   }
 
+  //Add URL handlers
   server.on("/", handleRoot);
   server.on("/manifest.json", handleManifest);
-
   server.onNotFound(handleNotFound);
 
+  //Begin networking
   server.begin();
   Serial.println("web server started");
-
   ws.begin();
   ws.onEvent(webSocketEvent);
   Serial.println("websocket server started");
 }
 
 void loop() {
+  //Update web server, web sockets, Thinger, and relay
   ws.loop();
   server.handleClient();
   thing.handle();
