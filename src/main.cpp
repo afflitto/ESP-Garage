02b154b02a5b3f1abdb9426a89d36bc31cae1706
LegoMaster616 <andrew.afflitto@gmail.com>
2017-10-10 22:24:13 -0400

    added basic websocket support

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,39 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <Hash.h>
+#include <WebSocketsServer.h>
+#include "pages.h"
 
 const char* ssid = "";
 const char* password = "";
 MDNSResponder mdns;
 
 ESP8266WebServer server(80);
+WebSocketsServer ws = WebSocketsServer(81);
 
 void handleRoot() {
   Serial.println("root");
-  server.send(200, "text/plain", "hello world");
+  server.send(200, "text/html", rootHTML);
 }
 
 void handleNotFound() {
   Serial.println("not found");
   server.send(404, "text/plain", "404: not found");
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.println("ws disconnected");
+      break;
+    case WStype_CONNECTED:
+      Serial.println("ws connected");
+      break;
+    case WStype_TEXT:
+      Serial.printf("ws recieved: %s\n", payload);
+      break;
+  }
 }
 
 void setup() {
@@ -43,9 +61,14 @@ void setup() {
   server.onNotFound(handleNotFound);
 
   server.begin();
-  Serial.println("server started");
+  Serial.println("web server started");
+
+  ws.begin();
+  ws.onEvent(webSocketEvent);
+  Serial.println("websocket server started");
 }
 
 void loop() {
+  ws.loop();
   server.handleClient();
 }
---
+++ src/pages.h
@@ -0,0 +1,25 @@
+String rootHTML = R"=====(
+  <!DOCTYPE HTML>
+  <html>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+      <title>Garage Door Opener</title>
+    </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="display-1">Garage Door Opener</h1>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-md-4 col-md-offset-4">
+            <a href="#" class="btn btn-block btn-lg btn-danger">Open/close</a>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+)=====";
